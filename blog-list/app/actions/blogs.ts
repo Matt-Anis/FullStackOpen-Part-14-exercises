f75f304599de8a9b5dd367d5c5c58de62f45be5e
Matt Anis <webdev.omis@gmail.com>
2026-08-03 12:55:35 +0100

    14. Blog creation form on error

--- blog-list/app/actions/blogs.ts
+++ blog-list/app/actions/blogs.ts
@@ -4,25 +4,46 @@ import { redirect } from "next/navigation";
 import { revalidatePath } from "next/cache";
 import { addBlog, likeBlog as updateBlogLikes } from "../services/blogs";
 
+type BlogFormState = {
+  errors: {
+    title?: string;
+    author?: string;
+    url?: string;
+  };
+  values?: {
+    title: string;
+    author: string;
+    url: string;
+  };
+};
+
 export const createBlog = async (
-  prevState: { error: string },
+  prevState: BlogFormState,
   formData: FormData,
-) => {
+): Promise<BlogFormState> => {
   const title = formData.get("title") as string;
   const author = formData.get("author") as string;
   const url = formData.get("url") as string;
-  if (
-    !title ||
-    title.length < 5 ||
-    !author ||
-    author.length < 5 ||
-    !url ||
-    url.length < 5
-  ) {
+
+  if (!title || title.length < 5) {
     return {
-      error: "minimum length for title, author and url is 5 characters long",
+      errors: { title: "minimum length for title is 5 characters long" },
+      values: { title, author, url },
     };
   }
+  if (!author || author.length < 5) {
+    return {
+      errors: { author: "minimum length for author is 5 characters long" },
+      values: { title, author, url },
+    };
+  }
+  if (!url || url.length < 5) {
+    return {
+      errors: { url: "minimum length for url is 5 characters long" },
+      values: { title, author, url },
+    };
+  }
+
   await addBlog(title, author, url);
 
   revalidatePath("/blogs");
--- blog-list/app/blogs/new/page.tsx
+++ blog-list/app/blogs/new/page.tsx
@@ -12,25 +12,51 @@ const NewBlog = () => {
     redirect("/login");
   }
 
-  const [state, formAction] = useActionState(createBlog, { error: "" });
+  const [state, formAction] = useActionState(createBlog, { errors: {} });
 
   return (
     <div>
       <h1>Create new</h1>
       <form action={formAction}>
         <div>
-          title: <input name="title" required minLength={5} />
+          title:{" "}
+          <input
+            name="title"
+            required
+            minLength={1}
+            defaultValue={state.values?.title}
+          />
+          {state.errors.title && (
+            <p style={{ color: "red" }}>{state.errors.title}</p>
+          )}
         </div>
         <div>
           <div>
-            author: <input name="author" required minLength={5} />
+            author:{" "}
+            <input
+              name="author"
+              required
+              minLength={1}
+              defaultValue={state.values?.author}
+            />
+            {state.errors.author && (
+              <p style={{ color: "red" }}>{state.errors.author}</p>
+            )}
           </div>
           <div>
-            url: <input name="url" required minLength={5} />
+            url:{" "}
+            <input
+              name="url"
+              required
+              minLength={1}
+              defaultValue={state.values?.url}
+            />
+            {state.errors.url && (
+              <p style={{ color: "red" }}>{state.errors.url}</p>
+            )}
           </div>
         </div>
         <button type="submit">create</button>
-        {state.error && <p style={{ color: "red" }}>{state.error}</p>}
       </form>
     </div>
   );
