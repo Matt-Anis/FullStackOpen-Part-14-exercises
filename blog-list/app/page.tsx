db81664797a34c6d99b9b602c26ddd0af4867ae1
Matt Anis <webdev.omis@gmail.com>
2026-08-05 09:24:06 +0100

    22. Static homepage from markdown

--- blog-list/app/page.tsx
+++ blog-list/app/page.tsx
@@ -1,12 +1,9 @@
+import Homepage from "./homepage.mdx";
+
 const Home = () => {
   return (
-    <div className="w-full flex flex-col gap-4 items-center justify-center ">
-      <h2 className="text-5xl font-bold font-mono capitalize">
-        Welcome to blogist
-      </h2>
-      <h4 className="font-mono">
-        browse blogs, create new ones and connect with other users
-      </h4>
+    <div className="markdown">
+      <Homepage />
     </div>
   );
 };
